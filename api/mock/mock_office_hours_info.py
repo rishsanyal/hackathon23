ed67ff17a96d7b43d36a5fe2f272c20b2f984f40
bharathradhakrishnan <bradhakrishnan@dons.usfca.edu>
2023-04-01 19:54:06 -0700

    Added end time

--- api/mock/mock_office_hours_info.py
+++ api/mock/mock_office_hours_info.py
@@ -12,10 +12,14 @@ date_string = curr_date_obj.isoformat()
 
 # Create timedelta objects for adding 24 hours and 3 days
 one_day = timedelta(days=1)
+one_hour = timedelta(hours=1)
 three_days = timedelta(days=3)
 
 # Increment time by 24 hours
 new_time_obj = curr_time_obj + one_day
+
+# Increment time by 1 hour
+new_time_obj2 = curr_time_obj + one_hour
 
 # Increment date by 3 days
 new_date_obj = curr_date_obj + three_days
@@ -25,18 +29,21 @@ MOCK_OFFICE_HOURS_INFO = {
         {
             "join": True,
             "time": str(new_time_obj).split(" ")[1],
+            "end_time": str(new_time_obj2).split(" ")[1],
             "date": new_date_obj.isoformat(),
             "zoom": zoom_link
         },
         {
             "join": True,
             "time": str(new_time_obj).split(" ")[1],
+            "end_time": str(new_time_obj2).split(" ")[1],
             "date": (new_date_obj + three_days).isoformat(),
             "zoom": zoom_link
         },
         {
             "join": True,
             "time": str(new_time_obj).split(" ")[1],
+            "end_time": str(new_time_obj2).split(" ")[1],
             "date": (new_date_obj + three_days + three_days).isoformat(),
             "zoom": zoom_link
         }
