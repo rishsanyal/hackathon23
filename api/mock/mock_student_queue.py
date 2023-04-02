373e79066bc29d8e9b443263ea38a967bc007400
Rishab Sanyal <rsanyal@ucdavis.edu>
2023-04-01 18:02:11 -0700

    adding mock info

--- api/mock/mock_student_queue.py
+++ api/mock/mock_student_queue.py
@@ -1,24 +1,53 @@
-# create a list 
+# create a list
 USER_INFO = [
     {
         'user_id': '1',
         'user_name': 'Jay',
         'is_curr_attendee': True,
+        'signed_up_for_oh_notification': True,
+        'signed_up_for_queue_notification': True,
+        'oh_notification_status': False,
+        'queue_notification_status': False,
+        'class_ids_for_notification': [
+            1, 2
+        ]
     },
     {
         'user_id': '2',
         'user_name': 'Rishab',
-        'is_curr_attendee': False
+        'is_curr_attendee': False,
+        'signed_up_for_oh_notification': True,
+        'signed_up_for_queue_notification': True,
+        'oh_notification_status': False,
+        'queue_notification_status': False,
+        'class_ids_for_notification': [
+            1, 2
+        ]
+
     },
     {
         'user_id': '3',
         'user_name': 'Tanya',
-        'is_curr_attendee': False
+        'is_curr_attendee': False,
+        'signed_up_for_oh_notification': True,
+        'signed_up_for_queue_notification': True,
+        'oh_notification_status': False,
+        'queue_notification_status': False,
+        'class_ids_for_notification': [
+            1, 2
+        ]
     },
     {
         'user_id': '4',
         'user_name': 'Bharath',
-        'is_curr_attendee': False
+        'is_curr_attendee': False,
+        'signed_up_for_oh_notification': True,
+        'signed_up_for_queue_notification': True,
+        'oh_notification_status': False,
+        'queue_notification_status': False,
+        'class_ids_for_notification': [
+            1, 2
+        ]
     }
 ]
 
