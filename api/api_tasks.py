ca9be69214c5e22d4ba6b1dd3c975df4d310a8a6
Rishab Sanyal <rsanyal@ucdavis.edu>
2023-06-22 16:17:37 -0700

    Adding tasks properly

--- api/api_tasks.py
+++ api/api_tasks.py
@@ -2,23 +2,22 @@ import os
 import time
 from celery import Celery
 
-
 CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379'),
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379')
 
-celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
+celery = Celery('api_tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
 
 
-# @celery.task(name='tasks.add')
-# def add(x: int, y: int) -> int:
-#     time.sleep(5)
-#     return x + y
+@celery.task(name='api_tasks.add')
+def add(x: int, y: int) -> int:
+    time.sleep(5)
+    return x + y
 
 celery.conf.beat_schedule = {
-    'add-every-60-seconds': {
-        'task': 'tasks.add',
+    'add-every-30-seconds': {
+        'task': 'api_tasks.add',
         'schedule': 30.0,
-        'args': (16, 30)
+        'args': (16, 16)
     },
 }
 
